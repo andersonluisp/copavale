4ca3633ed6c8f1e3fe700a6d085b39bc64198b5a
andersonluisp <andersonluiscp@gmail.com>
2021-06-06 12:36:43 -0300

    fix the MatchesFragment.kt and change colors of themes

--- app/src/main/java/com/andersonpimentel/myapplication/ui/matches/MatchesFragment.kt
+++ app/src/main/java/com/andersonpimentel/myapplication/ui/matches/MatchesFragment.kt
@@ -8,12 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.andersonpimentel.myapplication.databinding.FragmentGalleryBinding
+import com.andersonpimentel.myapplication.databinding.FragmentMatchesBinding
 
 class MatchesFragment : Fragment() {
 
     private lateinit var matchesViewModel: MatchesViewModel
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentMatchesBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -27,7 +27,7 @@ class MatchesFragment : Fragment() {
         matchesViewModel =
             ViewModelProvider(this).get(MatchesViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentMatchesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textGallery
