43ebf2408e36ceca0868e259f15db3deda6fe300
andersonluisp <andersonluiscp@gmail.com>
2021-06-09 20:57:15 -0300

    Removed some imports don't used

--- app/src/main/java/com/andersonpimentel/myapplication/ui/champs/TabMenuChampFragment.kt
+++ app/src/main/java/com/andersonpimentel/myapplication/ui/champs/TabMenuChampFragment.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TabHost
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.andersonpimentel.myapplication.R
 import com.andersonpimentel.myapplication.databinding.FragmentHomeBinding
 import com.andersonpimentel.myapplication.ui.home.HomeViewModel
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.view.*
 
 class TabMenuChampFragment : Fragment() {
