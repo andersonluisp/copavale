ae07059a41c7497491e1eec68b4fc73cc215743a
Anderson Pimentel <andersonluiscp@gmail.com>
2022-07-18 17:30:06 -0300

    update start koin

--- app/src/main/java/com/andersonpimentel/faceitdata/FaceitDataApplication.kt
+++ app/src/main/java/com/andersonpimentel/faceitdata/FaceitDataApplication.kt
@@ -2,6 +2,7 @@ package com.andersonpimentel.faceitdata
 
 import android.app.Application
 import com.andersonpimentel.faceitdata.login.data.di.loginDataModule
+import com.andersonpimentel.faceitdata.login.domain.di.loginDomainModule
 import com.andersonpimentel.faceitdata.login.presentation.di.loginPresentationModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -17,7 +18,8 @@ class FaceitDataApplication: Application() {
             androidContext(this@FaceitDataApplication)
             modules(
                 loginDataModule,
-                loginPresentationModule
+                loginPresentationModule,
+                loginDomainModule
             )
         }
     }
